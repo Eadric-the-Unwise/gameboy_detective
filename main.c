a441fc4fd4ecb724dc4174c8040ae5dfddca7c61
eric <fakeemail@example.com>
2021-05-07 10:11:03 +0900

    cleaned up void load_smoke_data

--- main.c
+++ main.c
@@ -60,15 +60,9 @@ void load_detective_data(Character *detective, UINT8 first_tile)
 
 void load_smoke_data(CharacterSmoke *smoke, UINT8 first_tile)
 {
-    // Body tiles
-    smoke->body_tile_index = first_tile;
-    // Number of tiles = (sizeof(tile_detectivewalk_data) >> 4)
-    set_sprite_data(smoke->body_tile_index, (sizeof(tile_detectivewalk_data) >> 4), tile_detectivewalk_data);
-    //cig_shine index
-    smoke->cig_shine_tile_index = smoke->body_tile_index + (sizeof(tile_detectivewalk_data) >> 4);
-    set_sprite_data(smoke->cig_shine_tile_index, (sizeof(cig_shine_data) >> 4), cig_shine_data);
-    //smoke index loads in the OAM from where the cig_shine is loading, + the number of tiles of cig_shine
-    smoke->smoke_tile_index = smoke->cig_shine_tile_index + (sizeof(cig_shine_data) >> 4);
+    first_tile += (sizeof(tile_detectivewalk_data) >> 4) + (sizeof(cig_shine_data) >> 4);
+    //smoke index loads in the OAM + the tile number of Detective + cig_shine
+    smoke->smoke_tile_index = first_tile;
     set_sprite_data(smoke->smoke_tile_index, (sizeof(smoke_data) >> 4), smoke_data);
 }
 //blocks detective from walking off of the screen
